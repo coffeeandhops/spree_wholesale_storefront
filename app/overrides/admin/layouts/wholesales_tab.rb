df371afb31f400b0b0105188b0cab1fd5d8e3894
Jason Kroll <jason@coffeeandhops.com>
2019-12-29 13:23:33 +1000

    Version 3 issues

--- app/overrides/admin/layouts/wholesales_tab.rb
+++
@@ -1,4 +0,0 @@
-Deface::Override.new(virtual_path: "spree/admin/shared/_main_menu",
-                    name: 'wholesalers-tab',
-                    insert_bottom: "nav",
-                    text: "<%= render 'spree/admin/wholesalers/tab' %>")
