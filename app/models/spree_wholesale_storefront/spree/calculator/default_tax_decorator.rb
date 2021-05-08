e75b15c6bc30094a20a25907d0527dab1dc6c37c
Jason Kroll <krolly@gmail.com>
2021-05-08 14:54:42 +1000

    fixed item

--- app/models/spree_wholesale_storefront/spree/calculator/default_tax_decorator.rb
+++ app/models/spree_wholesale_storefront/spree/calculator/default_tax_decorator.rb
@@ -7,9 +7,9 @@ module SpreeWholesaleStorefront
         def compute_shipment_or_line_item(item)
           amount = item.pre_tax_amount
           discount_amount = item.discounted_amount
-          if item.respond_to?(:order) && i.order.respond_to?(:is_wholesale) && i.order.is_wholesale
-            amount = item.i.wholesale_pre_tax_amount
-            discount_amount = item.i.discounted_wholesale_amount
+          if item.respond_to?(:order) && item.order.respond_to?(:is_wholesale) && item.order.is_wholesale
+            amount = item.wholesale_pre_tax_amount
+            discount_amount = item.discounted_wholesale_amount
            end
           if rate.included_in_price
             deduced_total_by_rate(amount, rate)
