2db0ab99946ffda9481d7729f1fa045dee003752
Jason Kroll <krolly@gmail.com>
2021-05-08 15:08:06 +1000

    debug

--- app/models/spree_wholesale_storefront/spree/calculator/default_tax_decorator.rb
+++ app/models/spree_wholesale_storefront/spree/calculator/default_tax_decorator.rb
@@ -10,6 +10,15 @@ module SpreeWholesaleStorefront
           if item.respond_to?(:order) && item.order.respond_to?(:is_wholesale) && item.order.is_wholesale
             amount = item.wholesale_pre_tax_amount
             discount_amount = item.discounted_wholesale_amount
+            pp "()()()()()()()()()()()()()()()()()()()()()()()()()()"
+            pp "()()()()()()()()()()()()()()()()()()()()()()()()()()"
+            pp "()()()()()()()()()()()()()()()()()()()()()()()()()()"
+            pp "In the right tax calculator"
+            pp amount
+            pp discount_amount
+            pp "()()()()()()()()()()()()()()()()()()()()()()()()()()"
+            pp "()()()()()()()()()()()()()()()()()()()()()()()()()()"
+            pp "()()()()()()()()()()()()()()()()()()()()()()()()()()"
            end
           if rate.included_in_price
             deduced_total_by_rate(amount, rate)
