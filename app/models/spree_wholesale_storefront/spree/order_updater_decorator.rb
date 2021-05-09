bf12ea5e3a680bd39df8c11a164a7afd5ddcca46
Jason Kroll <krolly@gmail.com>
2021-05-09 13:00:26 +1000

    moved wholesale changed check prior to item update

--- app/models/spree_wholesale_storefront/spree/order_updater_decorator.rb
+++ app/models/spree_wholesale_storefront/spree/order_updater_decorator.rb
@@ -11,7 +11,6 @@ module SpreeWholesaleStorefront
       # end
 
       def update_order_total
-        current_wholesale_status = order.is_wholesale
         order.total = order.item_total + order.shipment_total + order.adjustment_total
         order.wholesale_total = order.wholesale_item_total + order.shipment_total + order.adjustment_total
         order.is_wholesale = order.minimum_order?
@@ -20,6 +19,7 @@ module SpreeWholesaleStorefront
       end
 
       def update_item_total
+        current_wholesale_status = order.is_wholesale
         super
         order.wholesale_item_total = line_items.sum('wholesale_price * quantity')
         update_order_total
